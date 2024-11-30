d71770484f5f5f33eb12a46ec5fd8aa5abbe65f9
matsui-ale <matsui-alexandre@outlook.com>
2024-11-30 16:06:21 -0300

    Atualizando tabela Dynamo

--- src/FIAP.TechChallenge.LambdaPagamento.Domain/Entities/Pagamento.cs
+++ src/FIAP.TechChallenge.LambdaPagamento.Domain/Entities/Pagamento.cs
@@ -2,6 +2,7 @@
 using FIAP.TechChallenge.LambdaPagamento.Domain.Entities.Enum;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -9,6 +10,8 @@ using System.Threading.Tasks;
 
 namespace FIAP.TechChallenge.LambdaPagamento.Domain.Entities
 {
+    [ExcludeFromCodeCoverage]
+    [DynamoDBTable("PagamentoTable")]
     public class Pagamento
     {
         [DynamoDBHashKey("id")]
