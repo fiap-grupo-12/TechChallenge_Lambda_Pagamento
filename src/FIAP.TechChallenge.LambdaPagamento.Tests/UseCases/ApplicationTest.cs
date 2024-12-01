b938bf4dd162a2483fe7e99ba44e3120e8ff3956
fiap-grupo-12 <fiapgrupo12@outlook.com>
2024-12-01 08:58:18 -0300

    Ajuste teste

--- src/FIAP.TechChallenge.LambdaPagamento.Tests/UseCases/ApplicationTest.cs
+++ src/FIAP.TechChallenge.LambdaPagamento.Tests/UseCases/ApplicationTest.cs
@@ -3,7 +3,9 @@ using FIAP.TechChallenge.LambdaPagamento.Application.UseCases;
 using FIAP.TechChallenge.LambdaPagamento.Application.UseCases.Interfaces;
 using FIAP.TechChallenge.LambdaPagamento.Domain.Entities;
 using FIAP.TechChallenge.LambdaPagamento.Domain.Repositories;
+using FIAP.TechChallenge.LambdaPagamento.Domain.Repositories.MercadoPago;
 using FIAP.TechChallenge.LambdaPagamento.Infra.Data.Configurations;
+using FIAP.TechChallenge.LambdaPagamento.Infra.Data.Repositories.MercadoPago;
 using FIAP.TechChallenge.LambdaPagamento.Tests.Mock;
 using Moq;
 using System;
@@ -19,12 +21,14 @@ namespace FIAP.TechChallenge.LambdaPagamento.Tests.UseCases
     {
         private readonly Mock<ICriarPagamentoUseCase> _criarPagamento;
         private readonly Mock<IPagamentoRepository> _pagamentoRepository;
+        private readonly Mock<IMercadoPagoRepository> _mercadoPagoRepository;
         private readonly IMapper _mapper;
 
         public ApplicationTest()
         {
             _criarPagamento = new Mock<ICriarPagamentoUseCase>();
             _pagamentoRepository = new Mock<IPagamentoRepository>();
+            _mercadoPagoRepository = new Mock<IMercadoPagoRepository>();
             _mapper = new MapperConfiguration(c => c.AddProfile<MapperConfig>()).CreateMapper();
         }
 
@@ -35,9 +39,9 @@ namespace FIAP.TechChallenge.LambdaPagamento.Tests.UseCases
             var pagamento = PagamentoRequestMock.PagamentoRequestFake();
             var pagamentoFake = PagamentoRequestMock.PagamentoFake();
 
-            _pagamentoRepository.Setup(x => x.Post(It.IsAny<Pagamento>())).Returns(Task.FromResult(pagamentoFake)) ;
+            _pagamentoRepository.Setup(x => x.Post(It.IsAny<Pagamento>())).Returns(Task.FromResult(pagamentoFake));
 
-            var exec = new CriarPagamentoUseCase(_pagamentoRepository.Object, _mapper);
+            var exec = new CriarPagamentoUseCase(_pagamentoRepository.Object, _mercadoPagoRepository.Object, _mapper);
 
             // Act
             var result = await exec.Execute(pagamento);
